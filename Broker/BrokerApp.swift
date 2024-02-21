51581273f140efd836264f721e7f12629d97cd87
Weiming <wh249@njit.edu>
2024-02-20 21:44:16 -0500

    Initial Commit

---
+++ Broker/BrokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokerApp.swift
+//  Broker
+//
+//  Created by 何偉銘 on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BrokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
